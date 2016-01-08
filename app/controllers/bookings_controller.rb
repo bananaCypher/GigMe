137f7a5707d2080cb0fff23b1ee4444da277cbf6
Callum Hogg <bananaCypher@gmail.com>
2016-01-08 17:06:05 +0000

    Added the routes, controller and an index page for Booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,43 @@
+class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @bookings = current_user.bookings
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    booking = Booking.new(booking_params)
+    booking.user = current_user
+    booking.save
+    redirect_to bookings_path
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    booking = Booking.find(params[:id])
+    booking.update(booking_params)
+    redirect_to booking_path(booking)
+  end
+
+  def destroy
+    booking = Booking.find(params[:id])
+    booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:gig_id)
+  end
+end
