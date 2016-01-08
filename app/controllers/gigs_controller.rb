304d4fbbc5aea49410e442b17e83f1342e5cae1d
Callum Hogg <bananaCypher@gmail.com>
2016-01-08 15:57:04 +0000

    Added the Gigs Controller actions and set up the Index page

---
+++ app/controllers/gigs_controller.rb
@@ -0,0 +1,39 @@
+class GigsController < ApplicationController
+  def index
+    @gigs = Gig.all
+  end
+
+  def show
+    @gig = Gig.find(params[:id])
+  end
+
+  def new
+    @gig = Gig.new
+  end
+
+  def create
+    gig = Gig.create(gig_params)
+    redirect_to gig_path(gig)
+  end
+
+  def edit
+    @gig = Gig.find(params[:id])
+  end
+
+  def update
+    gig = Gig.find(params[:id])
+    gig.update(gig_params)
+    redirect_to gig_path(gig)
+  end
+
+  def destroy
+    gig = Gig.find(params[:id])
+    gig.destroy
+    redirect_to gigs_path
+  end
+
+  private
+  def gig_params
+    params.require(:gig).permit(:name, :price, :time, :venue_id)
+  end
+end
