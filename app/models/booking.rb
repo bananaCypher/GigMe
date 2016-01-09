ad8ebbde6a9cd82e826bae3cc9c8cb14089d3e18
Callum Hogg <bananaCypher@gmail.com>
2016-01-09 19:42:20 +0000

    Stylised the new Gig form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ActiveRecord::Base
   belongs_to :gig
 
   def self.user_booked_gig?(user, gig)
+    user ||= User.new
     Booking.where(gig_id: gig.id, user_id: user.id).count > 0
   end
 end
