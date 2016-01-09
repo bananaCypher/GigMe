051be1ba6118904df942c5b3e620f4b1e1ccd5bf
Callum Hogg <bananaCypher@gmail.com>
2016-01-09 16:54:06 +0000

    Set up the Venues controller and an index page.

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,39 @@
+class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.create(venue_params)
+    redirect_to veneus_path
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    venue = Venue.find(params[:id])
+    venue.update(venue_params)
+    redirect_to venue_path(venue)
+  end
+
+  def destroy
+    venue = Venue.find(params[:id])
+    venue.destroy
+    redirect_to venues_path
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:name, :location, :capacity)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'gigs#index'
   resources :gigs
   resources :bookings
+  resources :venues
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
