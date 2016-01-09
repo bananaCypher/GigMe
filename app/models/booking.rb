85e2573725f5b991c9a06056ab717bf18c7c1a0d
Callum Hogg <bananaCypher@gmail.com>
2016-01-09 18:50:13 +0000

    Added a warning before booking a gig you already have a ticket to

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :gig
+
+  def self.user_booked_gig?(user, gig)
+    Booking.where(gig_id: gig.id, user_id: user.id).count > 0
+  end
 end
