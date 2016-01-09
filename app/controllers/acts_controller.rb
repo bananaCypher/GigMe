72379794f2106a3694324f5cfe5bf1a405d40f89
Callum Hogg <bananaCypher@gmail.com>
2016-01-09 22:56:51 +0000

    Added some routes for acts and set up a skeleton Controller

---
+++ app/controllers/acts_controller.rb
@@ -0,0 +1,26 @@
+class ActsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def act_params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :gigs
   resources :bookings
   resources :venues
+  resources :acts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
