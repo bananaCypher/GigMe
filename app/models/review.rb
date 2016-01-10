19bef2fc20292b5860ae561fa8de5f9b1439de0c
Callum Hogg <bananaCypher@gmail.com>
2016-01-10 23:10:05 +0000

    Updated all the views to show actual reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,18 @@ class Review < ActiveRecord::Base
     validates :user_id, :gig_id, :rating, presence: true
     validates :rating, numericality: {greater_than: 0, less_than: 6}
     validates :content, length: {in: 2..2000}
+
+    def self.gig_average_rating(gig)
+        reviews = gig.reviews
+        return 0 if reviews.count == 0
+        gig.reviews.inject(0) {|sum, review| sum += review.rating} / gig.reviews.count
+    end
+
+    def stars_range
+        (1..self.rating)
+    end
+
+    def no_stars_range
+        (1..(5 - rating))
+    end
 end
