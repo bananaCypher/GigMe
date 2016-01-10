83a074f8575eef571ab46a63e4f93d2fee986c45
Callum Hogg <bananaCypher@gmail.com>
2016-01-10 15:17:27 +0000

    Added a warning before user double books themselves

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,4 +8,9 @@ class Booking < ActiveRecord::Base
     user ||= User.new
     Booking.where(gig_id: gig.id, user_id: user.id).count > 0
   end
+  def self.is_double_booking?(user, gig)
+    user ||= User.new
+    overbook = user.bookings.detect {|b| gig.start_time >= b.gig.start_time and gig.start_time <= b.gig.end_time }
+    return true if overbook
+  end
 end
