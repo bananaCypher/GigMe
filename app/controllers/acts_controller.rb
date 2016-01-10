61bfb0740654e1f4dcf8873aa109d10eebc995b5
Callum Hogg <bananaCypher@gmail.com>
2016-01-10 00:11:32 +0000

    Added an Acts controller and index page.

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -1,26 +1,39 @@
 class ActsController < ApplicationController
   def index
+    @acts = Act.all
   end
 
   def new
+    @act = Act.new
   end
 
   def create
+    Act.create(act_params)
+    redirect_to acts_path
   end
 
   def show
+    @act = Act.find(params[:id])
   end
 
   def edit
+    @act = Act.find(params[:id])
   end
 
   def update
+    act = Act.find(params[:id])
+    act.update(act_params)
+    redirect_to act_path(act)
   end
 
   def destroy
+    act = Act.find(params[:id])
+    act.destroy
+    redirect_to acts_path
   end
 
   private
   def act_params
+    params.require(:act).permit(:name, :description, :image)
   end
 end
