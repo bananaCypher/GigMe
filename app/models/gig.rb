d5d36112892cd2efb017bfd44cdd9b3a795292fd
Callum Hogg <bananaCypher@gmail.com>
2016-01-11 00:56:14 +0000

    Refactored the Gig view page

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,88 +1,106 @@
 class Gig < ActiveRecord::Base
-  validates :price, :start_time, :end_time, :capacity, :act_id, :venue_id, presence: true
-  validates :price, :capacity, numericality: {greater_than_or_equal_to: 0}
+    validates :price, :start_time, :end_time, :capacity, :act_id, :venue_id, presence: true
+    validates :price, :capacity, numericality: {greater_than_or_equal_to: 0}
 
-  belongs_to :act
-  belongs_to :venue
+    belongs_to :act
+    belongs_to :venue
 
-  has_many :bookings
-  has_many :users, through: :bookings
-  has_many :reviews
+    has_many :bookings
+    has_many :users, through: :bookings
+    has_many :reviews
 
-  scope :available, -> { all.find_all { |gig|  gig.full? == false } }
+    scope :available, -> { all.find_all { |gig|  gig.full? == false } }
 
     def name
-    self.act.name
+        self.act.name
     end
+
     def name_date
-    "#{self.name} - #{self.pretty_start_date}"
+        "#{self.name} - #{self.pretty_start_date}"
     end
 
-  def available_spaces
-    self.capacity - self.users.count 
-  end
-
-  def full?
-    self.available_spaces <= 0
-  end
-
-  def self.search(term)
-    acts = Act.where('name LIKE ?', "%#{term}%")
-    keywords = Keyword.where('tag LIKE ?', "%#{term}%")
-    venues = Venue.where('name LIKE ?', "%#{term}%")
-    acts = acts + keywords.map{|k| k.acts}.flatten.uniq
-    (acts.map{|a| a.gigs}.flatten + venues.map{|v| v.gigs}.flatten).uniq
-  end
-
-  def self.upcoming
-    self.where("start_time >= ?", Time.now)
-  end
-
-  def pretty_start
-    pretty_time(self.start_time)
-  end
-
-  def pretty_end
-    pretty_time(self.end_time)
-  end
-
-  def pretty_start_date
-    pretty_date(self.start_time)
-  end
-
-  def pretty_end_date
-    pretty_date(self.end_time)
-  end
-
-  def full_start
-    full_time(self.start_time)
-  end
-
-  def full_end
-    full_time(self.end_time)
-  end
-
-
-  private
-  def pretty_time(time)
-    time.strftime("%d/%m/%Y %H:%M")
-  end
-
-  def pretty_date(time)
-    time.strftime("%d/%m/%Y")
-  end
-
-  def full_time(time)
-    case time.day
-      when 1
-        mod = 'st'
-      when 2
-        mod = 'nd'
-      when 3
-        mod = 'rd'
-      else
-        mod = 'th'
+    def paragraph
+        "#{self.act.name} are playing live at #{self.venue.name} on #{self.full_start}.  Tickets cost £#{self.price}, book yours now before they sell out."
     end
-    time.strftime("%-d#{mod} of %B %Y")
-  end
+
+    def average_rating
+        return 0 if self.reviews.count == 0
+        self.reviews.inject(0) {|sum, review| sum += review.rating} / self.reviews.count
+    end
+
+    def avg_rating_stars_range
+        (1..self.average_rating)
+    end
+
+    def avg_rating_no_stars_range
+        (1..(5 - self.average_rating)) 
+    end
+
+    def available_spaces
+        self.capacity - self.users.count 
+    end
+
+    def full?
+        self.available_spaces <= 0
+    end
+
+    def self.search(term)
+        acts = Act.where('name LIKE ?', "%#{term}%")
+        keywords = Keyword.where('tag LIKE ?', "%#{term}%")
+        venues = Venue.where('name LIKE ?', "%#{term}%")
+        acts = acts + keywords.map{|k| k.acts}.flatten.uniq
+        (acts.map{|a| a.gigs}.flatten + venues.map{|v| v.gigs}.flatten).uniq
+    end
+
+    def self.upcoming
+        self.where("start_time >= ?", Time.now)
+    end
+
+    def pretty_start
+        pretty_time(self.start_time)
+    end
+
+    def pretty_end
+        pretty_time(self.end_time)
+    end
+
+    def pretty_start_date
+        pretty_date(self.start_time)
+    end
+
+    def pretty_end_date
+        pretty_date(self.end_time)
+    end
+
+    def full_start
+        full_time(self.start_time)
+    end
+
+    def full_end
+        full_time(self.end_time)
+    end
+
+
+    private
+    def pretty_time(time)
+        time.strftime("%d/%m/%Y %H:%M")
+    end
+
+    def pretty_date(time)
+        time.strftime("%d/%m/%Y")
+    end
+
+    def full_time(time)
+        case time.day
+        when 1
+            mod = 'st'
+        when 2
+            mod = 'nd'
+        when 3
+            mod = 'rd'
+        else
+            mod = 'th'
+        end
+        time.strftime("%-d#{mod} of %B %Y")
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,34 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :bookings
-  has_many :gigs, through: :bookings
-  has_many :reviews
+    has_many :bookings
+    has_many :gigs, through: :bookings
+    has_many :reviews
 
-  def role?(role_to_check)
-    self.role.to_s == role_to_check.to_s
-  end
+    def role?(role_to_check)
+        self.role.to_s == role_to_check.to_s
+    end
+
+    def booking_warnings(gig)
+        message = ""
+        if booked_gig? gig
+            message = message + "You already have a ticket for this gig. "
+        end
+        if is_double_booking? gig
+            message = message + "You already have a ticket for another gig that clashes with this one. "
+        end
+        {confirm: message + "Are you sure you want to book this ticket?"}
+    end
+
+    def booked_gig?(gig)
+        self.bookings.where(gig_id: gig.id).count > 0
+    end
+
+    def is_double_booking?(gig)
+        self.bookings.detect {|b| gig.start_time >= b.gig.start_time and gig.start_time <= b.gig.end_time and b.gig != gig}
+    end
+
 end
