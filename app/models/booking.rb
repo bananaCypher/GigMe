30c1b6ff473b8762b34348ed81eefe13f58eeab6
Callum Hogg <bananaCypher@gmail.com>
2016-01-11 01:15:33 +0000

    refactored the model code

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,14 +3,4 @@ class Booking < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :gig
-
-  def self.user_booked_gig?(user, gig)
-    user ||= User.new
-    Booking.where(gig_id: gig.id, user_id: user.id).count > 0
-  end
-  def self.is_double_booking?(user, gig)
-    user ||= User.new
-    overbook = user.bookings.detect {|b| gig.start_time >= b.gig.start_time and gig.start_time <= b.gig.end_time }
-    return true if overbook
-  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -6,12 +6,6 @@ class Review < ActiveRecord::Base
     validates :rating, numericality: {greater_than: 0, less_than: 6}
     validates :content, length: {in: 2..2000}
 
-    def self.gig_average_rating(gig)
-        reviews = gig.reviews
-        return 0 if reviews.count == 0
-        gig.reviews.inject(0) {|sum, review| sum += review.rating} / gig.reviews.count
-    end
-
     def stars_range
         (1..self.rating)
     end
