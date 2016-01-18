4403f9bb591a8b7d15e1f457791e281b3107790d
Callum Hogg <bananaCypher@gmail.com>
2016-01-18 16:03:07 +0000

    Set up user mailer with signup e-mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
+    after_create :send_welcome_email
 
     has_many :bookings
     has_many :gigs, through: :bookings
@@ -41,5 +42,9 @@ class User < ActiveRecord::Base
 
     def paid_bookings
         self.bookings.where(status: 'paid')
+    end
+
+    def send_welcome_email
+        UserMailer.welcome_email(self).deliver
     end
 end
